1fc1799935d8e30f81b781027d576da58b5d7041
Greg <gheidorn@cars.com>
2018-12-11 16:53:53 -0600

    port<space>

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :hizzle, HizzleWeb.Endpoint,
   # http: [:inet6, port: System.get_env("PORT") || 4000],
-  http: [port:8080],
+  http: [port: 8080],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
